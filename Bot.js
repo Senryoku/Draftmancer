27789e8e6d71c228e08c5e53a0e1386b29f96b57
Nicolas Levaique <nicolas.levaique@gmail.com>
2020-04-12 16:35:58 +0200

    improve color bonus computation

--- Bot.js
+++ Bot.js
@@ -9,17 +9,33 @@ function Bot(name, id) {
 	this.cards = []; // For debugging mostly.
 	this.pickedColors = { W: 0, U: 0, R: 0, B: 0, G: 0 };
 
-	let getColorBonus = function (card, colors, pickNumber) {
+	let getColorBonus = function (card, pickedColors, pickNumber) {
 		let colorBonus = 0;
 		if (pickNumber > colorBonusStartPick) {
-			for (let color of card.color_identity) {
-				colorBonus += 0.35 * colors[color];
+			if (card.color_identity.length == 0) { // don't over punish artifacts
+				colorBonus += 0.5;
+			}
+			else {
+				let initialized = false;
+				for (let color of card.color_identity) {
+					let currentColorScore = 0.35 * pickedColors[color];
+					if (!initialized || (colorBonus > currentColorScore)) {
+						initialized = true;
+						colorBonus = currentColorScore;
+					}
+				}
 			}
 		}
 		else {
 			if (card.color_identity.length == 0) { // early in the draft, slightly prioritize artifacts
 				colorBonus += 0.3;
 			}
+			else if (card.color_identity.length > 1) { // early in the draft, slightly punish gold cards
+				colorBonus -= 0.5;
+			}
+		}
+		if (colorBonus > 3) {
+			colorBonus = 3;
 		}
 		return colorBonus;
 	}
