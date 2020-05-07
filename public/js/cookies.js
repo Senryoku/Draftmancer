884ca3622279ea8b9d3bc6e9754408c315caa063
Senryoku <maretverdant@gmail.com>
2020-05-08 01:17:54 +0200

    Fixed cookies

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -1,10 +1,10 @@
 function getCookie(cname, def = "") {
-	let name = cname + "=";
-	let decodedCookie = decodeURIComponent(document.cookie);
-	let ca = decodedCookie.split(';');
-	for(let i = 0; i < ca.length; i++) {
+	const name = cname + "=";
+	const decodedCookie = decodeURIComponent(document.cookie);
+	const ca = decodedCookie.split(";");
+	for (let i = 0; i < ca.length; i++) {
 		let c = ca[i];
-		while (c.charAt(0) == ' ') {
+		while (c.charAt(0) == " ") {
 			c = c.substring(1);
 		}
 		if (c.indexOf(name) == 0) {
@@ -14,16 +14,10 @@ function getCookie(cname, def = "") {
 	return def;
 }
 
-function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+function setCookie(name, value) {
+	document.cookie = name + "=" + (value || "") + ";max-age=31536000;path=/";
 }
 
-function eraseCookie(name) {   
-    document.cookie = name+'=; Max-Age=-99999999;';  
+function eraseCookie(name) {
+	document.cookie = name + "=; Max-Age=-99999999;";
 }
