9a6cbbb3819a6cfa1bd43616efaa53ff3847f5d7
Senryoku <maretverdant@gmail.com>
2020-05-21 17:58:28 +0200

    Escape shortcut for modals

--- public/js/draft.js
+++ public/js/draft.js
@@ -21,6 +21,17 @@ Vue.component("modal", {
 		close: function (e) {
 			if (e.target == e.currentTarget) this.$emit("close");
 		},
+		shortcuts: function (e) {
+			if (e.which === 27)
+				// Escape
+				this.$emit("close");
+		},
+	},
+	mounted: function () {
+		document.addEventListener("keydown", this.shortcuts);
+	},
+	beforeDestroy: function () {
+		document.removeEventListener("keydown", this.shortcuts);
 	},
 });
 
