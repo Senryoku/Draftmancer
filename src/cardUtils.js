75268dfa010dbadd8e49528210e12a38a4836e5b
Senryoku <maretverdant@gmail.com>
2020-05-22 10:02:24 +0200

    Cleanup

--- src/cardUtils.js
+++ src/cardUtils.js
@@ -1,12 +1,13 @@
 "use strict";
 
 const removeCardFromDict = function (c, dict) {
+	/* // Debug
 	if (!dict || !c || !Object.keys(dict).includes(c)) {
-		// FIXME: Should not be useful!
 		console.error(`removeCardFromDict: ${c} not in dictionary! Dict. dump:`);
 		console.error(dict);
 		return;
 	}
+	*/
 	dict[c] -= 1;
 	if (dict[c] == 0) delete dict[c];
 };
