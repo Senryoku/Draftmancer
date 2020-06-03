58c0e2fa0a85d7bb44af0872e1c610e04be78194
Senryoku <maretverdant@gmail.com>
2020-06-03 17:56:47 +0200

    Moved Patch Notes to their own file

--- public/js/draft.js
+++ public/js/draft.js
@@ -9,6 +9,28 @@ function orderColor(lhs, rhs) {
 	else return String(lhs.flat()).localeCompare(String(rhs.flat()));
 }
 
+Vue.component("patch-notes", {
+	template: `
+<ol class="patch-notes">
+	<li v-for="pn in notes">{{pn.date}}
+		<ul>
+			<li v-for="n in pn.notes">{{n}}</li>
+		</ul>
+	</li>
+</ol>
+`,
+	data: function () {
+		return {
+			notes: [],
+		};
+	},
+	mounted: function () {
+		fetch("data/PatchNotes.json")
+			.then((response) => response.json())
+			.then((json) => (this.notes = json));
+	},
+});
+
 const SwalCustomClasses = {
 	popup: "custom-swal-popup",
 	title: "custom-swal-title",
