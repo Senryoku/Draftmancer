10db1e42d167d6c328e34d5d54ab41ca08e3c8f8
Will Macrae <wmacrae@palantir.com>
2020-06-19 15:26:29 -0700

    bigger core set icons

--- client/public/img/sets/update-set-images.sh
+++ client/public/img/sets/update-set-images.sh
@@ -2,8 +2,6 @@
 
 count=$(ls -l | grep svg | wc -l)
 echo "Starting with $count svgs."
-#rm -v !(ana.svg|g18.svg|pana.svg|pdom.svg|pgrn.svg|prix.svg|sld.svg|update-set-images.sh) *.svg
-#echo "Removed old svgs"
 curl https://codeload.github.com/andrewgioia/Keyrune/zip/master -o master.zip
 echo "Downloaded zip with svgs"
 unzip master.zip 1>/dev/null
@@ -12,7 +10,9 @@ echo "Moved svgs out of zip"
 rm -rf Keyrune-master
 rm -f master.zip
 echo "Cleaned up other files"
-sed -i "" 's/width=".*" h/width="32" h/;s/fill="#444"//' *.svg
+sed -i "" 's/fill="#444"//' *.svg
+echo "Making core symbols bigger"
+sed -i "" 's/height=".*" viewBox="0 0 32 32">/height="16" viewBox="0 8 32 16">/' m[0-9][0-9].svg
 echo "Edited svgs to 32px width and white fill"
 count=$(ls -l | grep svg | wc -l)
 echo "Finished with $count svgs."
