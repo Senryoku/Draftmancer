Senryoku/Draftmancer

commit dfb95181f62e1fe1d34dd83e493c4558d7cfb633

Improved card dragging ghost on Chrome. Still broken the first time on Firefox (looks like it could be related to the style 'transform: translateY' automatically applied to the element (by vuedraggeable/sortablejs, I guess))

client/src/components/CardPool.vue
@@ -11,7 +11,7 @@
 			class="card-column drag-column"
 			:list="column"
 			:group="group"
-			@change="change"
+			drag-class="drag"
 		>
 			<card
 				v-for="card in column"
@@ -162,5 +162,9 @@ export default {
 	width: 32px;
 	height: 32px;
 	text-align: center;
+}
+
+.drag {
+	height: 283.33px;
 }
 </style>

commit c3d7570a0894557e7142ccb9ebe3b8bf2f050696

Fixed dumb regression introduced by dfb95181f62e1fe1d34dd83e493c4558d7cfb633 (That's a kind way to put it.)

client/src/components/CardPool.vue
@@ -11,6 +11,7 @@
 			class="card-column drag-column"
 			:list="column"
 			:group="group"
+			@change="change"
 			drag-class="drag"
 		>
 			<card