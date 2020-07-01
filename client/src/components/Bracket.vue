407396acffbf713c43430ce7bea5c6a85deb6632
Will Macrae <wmacrae@palantir.com>
2020-07-01 09:58:10 -0700

    first pass of swiss

--- client/src/components/Bracket.vue
+++ client/src/components/Bracket.vue
@@ -3,9 +3,10 @@
 	<div class="bracket" v-if="bracket">
 		<div class="bracket-column" v-for="(col, colIndex) in matches" :key="colIndex">
 			<div v-for="(m, matchIndex) in col" :key="matchIndex" class="bracket-match">
+				<div>{{m.index+1}}</div>
 				<div v-for="(p, index) in m.players" :key="index">
 					<div class="bracket-player bracket-empty" v-if="p.empty">(Empty)</div>
-					<div class="bracket-player bracket-tbd" v-else-if="p.tbd">(TBD)</div>
+					<div class="bracket-player bracket-tbd" v-else-if="p.tbd">(TBD {{p.tbd}})</div>
 					<div class="bracket-player" :class="{'bracket-winner': bracket.results[m.index][index] > bracket.results[m.index][(index + 1)%2]}" v-else>
 						<div class="bracket-player-name">{{p}}</div> 
 						<template v-if="m.isValid()">
@@ -54,10 +55,22 @@ export default {
 					!this.bracket.results ||
 					this.bracket.results[match.index][0] === this.bracket.results[match.index][1]
 				)
-					return { tbd: true };
+					return { tbd: "W" + (match.index + 1) };
 				if (this.bracket.results[match.index][0] > this.bracket.results[match.index][1])
 					return match.players[0];
 				else return match.players[1];
+			};
+
+			const loser = (match) => {
+				if (match.players[0].empty || match.players[1].empty) return { empty: true };
+				if (
+					!this.bracket.results ||
+					this.bracket.results[match.index][0] === this.bracket.results[match.index][1]
+				)
+					return { tbd: "L" + (match.index + 1) };
+				if (this.bracket.results[match.index][0] > this.bracket.results[match.index][1])
+					return match.players[1];
+				else return match.players[0];
 			};
 
 			for (let i = 0; i < 4; ++i) {
@@ -70,7 +83,12 @@ export default {
 			}
 			m[1].push(new Match(4, [winner(m[0][0]), winner(m[0][1])]));
 			m[1].push(new Match(5, [winner(m[0][2]), winner(m[0][3])]));
-			m[2].push(new Match(6, [winner(m[1][0]), winner(m[1][1])]));
+			m[1].push(new Match(6, [loser(m[0][2]), loser(m[0][3])]));
+			m[1].push(new Match(7, [loser(m[0][0]), loser(m[0][1])]));
+			m[2].push(new Match(8, [winner(m[1][0]), winner(m[1][1])]));
+			m[2].push(new Match(9, [loser(m[1][0]), winner(m[1][2])]));
+			m[2].push(new Match(10, [loser(m[1][1]), winner(m[1][3])]));
+			m[2].push(new Match(11, [loser(m[1][2]), loser(m[1][3])]));
 			return m;
 		},
 	},
