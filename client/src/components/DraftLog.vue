746215d6b962f7b6317728b0d6be71b936f81050
Senryoku <maretverdant@gmail.com>
2020-07-21 15:30:37 +0200

    Moving Draft Log css to the correct file; Removing unused rule

--- client/src/components/DraftLog.vue
+++ client/src/components/DraftLog.vue
@@ -191,3 +191,147 @@ export default {
 	},
 };
 </script>
+
+<style>
+ul.player-table {
+	display: flex;
+	flex-wrap: wrap;
+	list-style: none;
+	--margin: 1.5em;
+}
+
+ul.player-table li {
+	width: calc(24% - 2 * var(--margin) - 1em);
+	max-width: calc(24% - 2 * var(--margin) - 1em);
+	border: 1px solid black;
+	margin: var(--margin);
+	position: relative;
+	padding: 0.5em;
+	border-radius: 0.2em;
+}
+
+ul.player-table li:nth-child(1) {
+	order: 1;
+}
+ul.player-table li:nth-child(2) {
+	order: 2;
+}
+ul.player-table li:nth-child(3) {
+	order: 3;
+}
+ul.player-table li:nth-child(4) {
+	order: 4;
+}
+ul.player-table li:nth-child(5) {
+	order: 8;
+}
+ul.player-table li:nth-child(6) {
+	order: 7;
+}
+ul.player-table li:nth-child(7) {
+	order: 6;
+}
+ul.player-table li:nth-child(8) {
+	order: 5;
+}
+
+ul.player-table li:nth-child(1):after,
+ul.player-table li:nth-child(2):after,
+ul.player-table li:nth-child(3):after {
+	content: "";
+	height: 1px;
+	background: black;
+	width: calc(2 * var(--margin));
+	position: absolute;
+	right: calc(-2 * var(--margin));
+	top: 50%;
+}
+
+ul.player-table li:nth-child(1):before,
+ul.player-table li:nth-child(2):before,
+ul.player-table li:nth-child(3):before {
+	content: "";
+	position: absolute;
+	width: 0;
+	height: 0;
+	top: 50%;
+	border-style: solid;
+	border-width: 7px 0 7px 20px;
+	border-color: transparent transparent transparent black;
+	right: -20px;
+	transform: translateY(-50%) rotate(180deg);
+}
+
+ul.player-table li:nth-child(5):after,
+ul.player-table li:nth-child(6):after,
+ul.player-table li:nth-child(7):after {
+	content: "";
+	height: 1px;
+	background: black;
+	width: calc(2 * var(--margin));
+	position: absolute;
+	left: calc(-2 * var(--margin));
+	top: 50%;
+}
+
+ul.player-table li:nth-child(5):before,
+ul.player-table li:nth-child(6):before,
+ul.player-table li:nth-child(7):before {
+	content: "";
+	position: absolute;
+	width: 0;
+	height: 0;
+	top: 50%;
+	border-style: solid;
+	border-width: 7px 0 7px 20px;
+	border-color: transparent transparent transparent black;
+	left: -20px;
+	transform: translateY(-50%);
+}
+
+ul.player-table li:nth-child(8):before {
+	content: "";
+	position: absolute;
+	width: 0;
+	height: 0;
+	left: 50%;
+	border-style: solid;
+	border-width: 7px 0 7px 20px;
+	border-color: transparent transparent transparent black;
+	top: -20px;
+	transform: translateX(-50%) rotate(90deg);
+}
+
+ul.player-table li:nth-child(8):after {
+	content: "";
+	width: 1px;
+	background: black;
+	height: calc(2 * var(--margin));
+	position: absolute;
+	top: calc(-2 * var(--margin));
+	left: 50%;
+}
+
+ul.player-table li:nth-child(4):before {
+	content: "";
+	position: absolute;
+	width: 0;
+	height: 0;
+	left: 50%;
+	border-style: solid;
+	border-width: 7px 0 7px 20px;
+	border-color: transparent transparent transparent black;
+	bottom: -20px;
+	transform: translateX(-50%) rotate(-90deg);
+}
+
+ul.player-table li:nth-child(4):after {
+	content: "";
+	width: 1px;
+	background: black;
+	height: calc(2 * var(--margin));
+	position: absolute;
+	bottom: calc(-2 * var(--margin));
+	left: 50%;
+}
+</style>
