224f48fcd7853d98938e238abd8bd9ca9e52bec3
Senryoku <maretverdant@gmail.com>
2021-04-04 11:28:50 +0200

    Data update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1243,8 +1243,7 @@
 						<div class="welcome-section">
 							<div class="news">
 								<em>April 04, 2021</em>
-								<p><img src="img/sets/stx.svg" class="set-icon" style="--invertedness: 100%" /> Strixhaven: School of Mages support!<br />
-								<i class="fas fa-exclamation-triangle yellow"></i> Some cards may still be in low quality, not available in english or missing entirely. This will be resolved in the upcoming days.</p>
+								<p><img src="img/sets/stx.svg" class="set-icon" style="--invertedness: 100%" /> Strixhaven: School of Mages support!</p>
 							</div>
 							<div class="news">
 								<em>March 28, 2021</em>
