972a780a4c07ce63decc25f4f73b08ddd2d6db4f
Senryoku <maretverdant@gmail.com>
2021-05-21 01:34:43 +0200

    STX set rule test

--- test/setrules.js
+++ test/setrules.js
@@ -35,13 +35,13 @@ describe("Set Specific Booster Rules", function() {
 	const validateSTXBooster = function(booster) {
 		expect(booster.map(c => c.set).every(s => s === "stx" || s === "sta")).to.be.true;
 		let ArchiveCount = booster.reduce((acc, val) => {
-			return acc + val.set === "sta" ? 1 : 0;
+			return acc + (val.set === "sta" ? 1 : 0);
 		}, 0);
 		expect(ArchiveCount).to.equal(1);
 		let LessonCount = booster.reduce((acc, val) => {
 			return acc + val.subtypes.includes("Lesson") ? 1 : 0;
 		}, 0);
-		expect(LessonCount).to.equal(1);
+		expect(LessonCount).to.be.within(1, 2);
 	};
 
 	beforeEach(function(done) {
@@ -108,6 +108,7 @@ describe("Set Specific Booster Rules", function() {
 	testSet("dom", validateDOMBooster, "at least one legendary creature per pack");
 	testSet("war", validateWARBooster, "exactly one planeswalker per pack");
 	testSet("znr", validateZNRBooster, "exactly one MDFC per pack");
+	testSet("stx", validateSTXBooster, "exactly one STA and 1 or 2 lesson(s) per pack");
 
 	it(`Validate mixed Custom boosters.`, function(done) {
 		let ownerIdx = clients.findIndex(c => c.query.userID == Sessions[sessionID].owner);
