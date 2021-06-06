ab97226903f309b5f8cc86214af9ecad106feddd
Senryoku <maretverdant@gmail.com>
2021-06-06 18:05:53 +0200

    Fix frontend build

--- src/parseCost.ts
+++ src/parseCost.ts
@@ -8,7 +8,7 @@ export default function parseCost(cost: string) {
 	if (!cost || cost === "") return r;
 	// Use only the first part of split cards
 	if (cost.includes("//")) cost = cost.split("//")[0].trim();
-	let symbols = cost.match(/({[^}]+})/g) ?? [];
+	let symbols = cost.match(/({[^}]+})/g) || [];
 	for (let s of symbols) {
 		r.cmc += (ManaSymbols as any)[s].cmc;
 		r.colors = r.colors.concat((ManaSymbols as any)[s].colors);
