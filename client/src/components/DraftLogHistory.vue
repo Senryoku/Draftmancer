2b4168a98e539bf776995e375aaa4a85810fa4a6
tooomm <tooomm@users.noreply.github.com>
2021-08-08 00:53:51 +0200

    Update DraftLogHistory.vue

--- client/src/components/DraftLogHistory.vue
+++ client/src/components/DraftLogHistory.vue
@@ -21,7 +21,7 @@
 		<div v-for="(draftLog, idx) in orderedLogs" :key="idx" class="log">
 			<div class="log-controls">
 				<span @click="toggle(idx)" class="clickable" v-bind:class="{ disabled: draftLog.delayed }">
-					<div class="inline" style="min-width: 20px">
+					<div class="inline" style="min-width: 20px; text-align: center">
 						<i
 							v-if="!draftLog.delayed"
 							class="fa"
