47ae46317418601cc82c5102465e9b0128187051
Senryoku <maretverdant@gmail.com>
2021-09-15 01:36:41 +0200

    MID card rating

--- ManageCardData.py
+++ ManageCardData.py
@@ -217,7 +217,7 @@ if not os.path.isfile(RatingsDest) or ForceRatings:
         with open(path, 'r', encoding="utf8") as file:
             text = file.read()
             matches = re.findall(
-                r"<h3>([^<]+)<\/h3>\s*<b>Pro Rating: ([0-9]*\.?[0-9]*)<\/b>", text)
+                r"<h3>([^<]+)<\/h3>\s*<b>Pro Rating: ([0-9]*\.?[0-9]*( \/\/ )?[0-9]*\.?[0-9]*)<\/b>", text)
             print("Extracting ratings from ", path,
                   ": Found ", len(matches), " matches.")
             for m in matches:
