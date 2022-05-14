326c954fa0da97c752b443537025e5a419db8b56
Senryoku <maretverdant@gmail.com>
2022-05-14 13:50:45 +0200

    Bullet points

--- client/src/components/Esporter.vue
+++ client/src/components/Esporter.vue
@@ -1,16 +1,26 @@
 <template>
 	<div>
-		<div class="section-title">
-			<h2>Coaching</h2>
-		</div>
 		<a href="https://esporter.win/mtgadraft" target="_blank">
 			<div class="welcome-section welcome-alt">
-				<img src="../assets/img/logo-nobackground-500-cropped.png" alt="Esporter" />
-				<div v-if="remainingTime">
-					Next Split starts in <span v-if="remainingTime.days > 0">{{ remainingTime.days }}d</span>
-					{{ remainingTime.hours.toString().padStart(2, "0") }}h
-					{{ remainingTime.minutes.toString().padStart(2, "0") }}m
-					{{ remainingTime.seconds.toString().padStart(2, "0") }}s
+				<img src="../assets/img/logo-nobackground-500-cropped.png" alt="Esporter Logo" />
+				<div>
+					Esporter's Splits are the best way to improve at Magic!
+					<ol>
+						<li><strong>Group Coaching</strong> sessions with a Pro</li>
+						<li><strong>Scrimmages</strong> with other players in your Split</li>
+						<li><strong>Gameplay reviews</strong> with our resident Esporter coaches</li>
+						<li>A <strong>Championship Tournament</strong> to conclude the Split</li>
+					</ol>
+				</div>
+				<div v-if="remainingTime" style="margin: 0.2em 0.2em 0 0.2em">
+					<div style="text-align: right">
+						<div style="float: left">Next Split:</div>
+						Led by <strong>JustLolaman</strong><br />
+						Starts in <span v-if="remainingTime.days > 0">{{ remainingTime.days }}d</span>
+						{{ remainingTime.hours.toString().padStart(2, "0") }}h
+						{{ remainingTime.minutes.toString().padStart(2, "0") }}m
+						{{ remainingTime.seconds.toString().padStart(2, "0") }}s
+					</div>
 				</div>
 			</div></a
 		>
@@ -31,7 +41,7 @@ function computeRemainingTime(targetDate) {
 }
 export default {
 	data: () => {
-		const nextSplitDate = new Date("May 9, 2022 4:00 pm PST");
+		const nextSplitDate = new Date("May 30, 2022 4:00 pm PST");
 		let remainingTime = computeRemainingTime(nextSplitDate);
 		return {
 			interval: null,
@@ -56,5 +66,10 @@ export default {
 a,
 a:visited {
 	color: #ddd;
+}
+
+ol {
+	list-style: none;
+	margin: 0;
 }
 </style>
