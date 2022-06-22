81593e0856071d3512718f344dcbf5b47770f3f3
Senryoku <maretverdant@gmail.com>
2022-06-22 20:22:50 +0200

    Buggy rochester draft front-end test

---
+++ test/frontend/rochester.js
@@ -0,0 +1,68 @@
+import { describe, it } from "mocha";
+import chai from "chai";
+const expect = chai.expect;
+import { sessionOwnerPage, otherPlayerPage, waitAndClickXpath, waitAndClickSelector } from "./src/common.js";
+
+async function pickRochester(page) {
+	let next = await page.waitForXPath(
+		"//div[contains(., 'Done drafting!')] | //span[contains(., 'Pick a card')] | //span[contains(., 'Waiting for')]"
+	);
+	let text = await page.evaluate((next) => next.innerText, next);
+	if (text === "Done drafting!") return true;
+	if (text.includes("Waiting for")) return false;
+
+	const cards = await page.$$(".booster-card");
+	if (cards.length === 0) return false;
+	const card = cards[Math.floor(Math.random() * cards.length)];
+	expect(card).to.exist;
+	await card.click();
+	await waitAndClickSelector(page, 'input[value="Confirm Pick"]');
+	return false;
+}
+
+// Buggy: The draft isn't launched automatically
+describe("Rochester", function () {
+	this.timeout(100000);
+	it("Owner joins", async function () {
+		await sessionOwnerPage.goto(`http://localhost:${process.env.PORT}`);
+	});
+
+	it(`Another Player joins the session`, async function () {
+		// Get session link
+		await sessionOwnerPage.$$(".fa-share-square");
+		await sessionOwnerPage.click(".fa-share-square");
+		let clipboard = await sessionOwnerPage.evaluate(() => navigator.clipboard.readText());
+		expect(clipboard).to.match(/^http:\/\/localhost:3001\/\?session=/);
+
+		await otherPlayerPage.goto(clipboard);
+	});
+
+	it(`Launch Draft`, async function () {
+		// No idea why this one doesn't work.
+		await waitAndClickXpath(sessionOwnerPage, "//button[contains(., 'Rochester')]");
+		await waitAndClickXpath(sessionOwnerPage, "//button[contains(., 'Rochester')]");
+
+		await sessionOwnerPage.waitForXPath("//h2[contains(., 'Rochester Draft')]", {
+			visible: true,
+		});
+		await otherPlayerPage.waitForXPath("//h2[contains(., 'Rochester Draft')]", {
+			visible: true,
+		});
+		await sessionOwnerPage.waitForXPath("//div[contains(., 'Now drafting!')]", {
+			hidden: true,
+		});
+		await otherPlayerPage.waitForXPath("//div[contains(., 'Now drafting!')]", {
+			hidden: true,
+		});
+	});
+
+	it(`Pick until done.`, async function () {
+		let done = false;
+		while (!done) {
+			await sessionOwnerPage.waitForTimeout(100); // No idea why it's needed here.
+			let ownerPromise = pickRochester(sessionOwnerPage);
+			let otherPromise = pickRochester(otherPlayerPage);
+			done = (await ownerPromise) && (await otherPromise);
+		}
+	});
+});
---
+++ test/frontend/src/common.js
@@ -0,0 +1,93 @@
+import { before, after, beforeEach, afterEach } from "mocha";
+import puppeteer from "puppeteer";
+import chai from "chai";
+const expect = chai.expect;
+import { enableLogs, disableLogs } from "../../src/common.js";
+
+export let ownerBrowser;
+export let sessionOwnerPage;
+export let otherBrowser;
+export let otherPlayerPage;
+
+const testDebug = true; // Display tests for debugging
+const debugWindowWidth = 960;
+const debugWindowHeight = 1080;
+
+export async function waitAndClickXpath(page, xpath) {
+	const element = await page.waitForXPath(xpath, {
+		visible: true,
+	});
+	expect(element).to.exist;
+	await element.click();
+}
+
+export async function waitAndClickSelector(page, selector) {
+	await page.waitForSelector(selector, {
+		visible: true,
+	});
+	const [element] = await page.$$(selector);
+	expect(element).to.exist;
+	await element.click();
+}
+
+function disableAnimations(page) {
+	page.on("load", () => {
+		const content = `
+		*,
+		*::after,
+		*::before {
+			transition-delay: 0s !important;
+			transition-duration: 0s !important;
+			animation-delay: -0.0001s !important;
+			animation-duration: 0s !important;
+			animation-play-state: paused !important;
+			caret-color: transparent !important;
+		}`;
+
+		page.addStyleTag({ content });
+	});
+}
+
+before(async function () {
+	ownerBrowser = await puppeteer.launch({
+		headless: !testDebug,
+		args: testDebug
+			? [`--window-size=${debugWindowWidth},${debugWindowHeight}`, `--window-position=0,0`, "--mute-audio"]
+			: [],
+	});
+	otherBrowser = await puppeteer.launch({
+		headless: !testDebug,
+		args: testDebug
+			? [
+					`--window-size=${debugWindowWidth},${debugWindowHeight}`,
+					`--window-position=${debugWindowWidth},0`,
+					"--mute-audio",
+			  ]
+			: [],
+	});
+	const context = ownerBrowser.defaultBrowserContext();
+	context.overridePermissions(`http://localhost:${process.env.PORT}`, ["clipboard-read"]);
+	[sessionOwnerPage] = await ownerBrowser.pages();
+	[otherPlayerPage] = await otherBrowser.pages();
+	if (testDebug) {
+		sessionOwnerPage.setViewport({ width: debugWindowWidth, height: debugWindowHeight });
+		otherPlayerPage.setViewport({ width: debugWindowWidth, height: debugWindowHeight });
+	}
+	disableAnimations(sessionOwnerPage);
+	disableAnimations(otherPlayerPage);
+});
+
+after(async function () {
+	await ownerBrowser.close();
+	await otherBrowser.close();
+});
+
+beforeEach(function (done) {
+	disableLogs();
+	done();
+});
+
+afterEach(function (done) {
+	enableLogs(this.currentTest.state == "failed");
+	done();
+});
