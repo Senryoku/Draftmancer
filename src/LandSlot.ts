33c4bbcd5ac996e0f2ea028cbb667d967699ae4c
Senryoku <maretverdant@gmail.com>
2022-06-25 19:23:04 +0200

    Fix 2x2 land slot

--- src/LandSlot.ts
+++ src/LandSlot.ts
@@ -195,7 +195,9 @@ export const SpecialLandSlots: { [set: string]: SpecialLandSlot } = {
 		5 / 12 // FIXME: This is arbitrary, we don't know the actual rate for now.
 	),
 	// Double Masters 2022: Always a Cryptic Spires
-	"2x2": new SpecialLandSlot("2x2", ["309a6684-ecb3-491c-899a-3aa15a51130b"], 1, []),
+	"2x2": new SpecialLandSlot("2x2", ["309a6684-ecb3-491c-899a-3aa15a51130b"], 1, [
+		"309a6684-ecb3-491c-899a-3aa15a51130b",
+	]),
 };
 
 export const BasicLandSlots: { [set: string]: BasicLandSlot } = {};
