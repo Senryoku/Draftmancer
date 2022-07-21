5c8cd2cb94c3986ebb70b9827f78ce4276527abb
Senryoku <maretverdant@gmail.com>
2022-07-21 11:24:53 +0200

    HBG: Exclude previous versions of rebalanced cards from packs.

--- ManageCardData.py
+++ ManageCardData.py
@@ -416,6 +416,10 @@ if not os.path.isfile(FinalDataPath) or ForceCache or FetchSet:
             if c['id'] == "0826e210-2002-43fe-942d-41922dfd7bc2":
                 selection['in_booster'] = False
 
+            # Workaround: Remove cards from HBG that received a rebalanced version from packs
+            if c['id'] in ["057c66a8-9424-4c88-9707-5d8ef9170119", "e07d5fd5-d513-46d4-8812-6e6e55a6dfda", "a5cbda07-53a0-4526-9955-36f902073cf1", "ea4f1d5d-7991-4a2d-b907-3522e951ad4c", "884565bb-ed33-4372-8c81-487c2ee2f73c"]:
+                selection['in_booster'] = False
+
             if c['layout'] == "split":
                 if 'Aftermath' in c['keywords']:
                     selection['layout'] = 'split-left'
