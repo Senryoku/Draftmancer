8d4a50e594eb812888188d49f5ae1ed6379ea6bf
Senryoku <maretverdant@gmail.com>
2022-10-11 18:55:34 +0200

    webp

--- client/webpack.config.cjs
+++ client/webpack.config.cjs
@@ -29,7 +29,7 @@ module.exports = {
 				use: ["vue-style-loader", "css-loader"],
 			},
 			{
-				test: /\.(png|svg|jpg|gif)$/,
+				test: /\.(png|svg|jpg|gif|webp)$/,
 				type: "asset/resource",
 			},
 			{
