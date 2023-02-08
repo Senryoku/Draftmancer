ab47d2b8bf4432b303ad768f87ca9db423cf5b15
Senryoku <maretverdant@gmail.com>
2023-02-08 10:35:06 +0100

    Bunch of const

--- src/parseCost.ts
+++ src/parseCost.ts
@@ -4,15 +4,15 @@ type parseCostReturnType = { cmc: number; colors: string[] };
 const ManaSymbols: { [key: string]: { cmc: number | null; colors: string[] } } = ManaSymbolsJSON;
 
 export default function parseCost(cost: string) {
-	let r: parseCostReturnType = {
+	const r: parseCostReturnType = {
 		cmc: 0,
 		colors: [],
 	};
 	if (!cost || cost === "") return r;
 	// Use only the first part of split cards
 	if (cost.includes("//")) cost = cost.split("//")[0].trim();
-	let symbols = cost.match(/({[^}]+})/g) ?? [];
-	for (let s of symbols) {
+	const symbols = cost.match(/({[^}]+})/g) ?? [];
+	for (const s of symbols) {
 		r.cmc += ManaSymbols[s].cmc ?? 0;
 		r.colors = r.colors.concat(ManaSymbols[s].colors);
 	}
