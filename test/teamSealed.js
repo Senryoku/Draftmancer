584cdea5096174d2e0788f00589feab2ba9e501f
Senryoku <maretverdant@gmail.com>
2023-02-12 23:55:40 +0100

    [WIP] Tests

---
+++ test/teamSealed.js
@@ -0,0 +1,91 @@
+"use strict";
+import { before, after, beforeEach, afterEach, describe, it } from "mocha";
+import chai from "chai";
+const expect = chai.expect;
+import { Sessions } from "../dist/Session.js";
+import { Connections } from "../dist/Connection.js";
+import { makeClients, enableLogs, disableLogs, waitForClientDisconnects } from "./src/common.js";
+
+describe("Team Sealed", function () {
+	let clients = [];
+	let sessionID = "sessionID";
+	let ownerIdx;
+
+	beforeEach(function (done) {
+		disableLogs();
+		done();
+	});
+
+	afterEach(function (done) {
+		enableLogs(this.currentTest.state == "failed");
+		done();
+	});
+
+	before(function (done) {
+		clients = makeClients(
+			[
+				{
+					userID: "id1",
+					sessionID: sessionID,
+					userName: "Client1",
+				},
+				{
+					userID: "id2",
+					sessionID: sessionID,
+					userName: "Client2",
+				},
+				{
+					userID: "id3",
+					sessionID: sessionID,
+					userName: "Client3",
+				},
+				{
+					userID: "id4",
+					sessionID: sessionID,
+					userName: "Client4",
+				},
+				{
+					userID: "id5",
+					sessionID: sessionID,
+					userName: "Client5",
+				},
+				{
+					userID: "id6",
+					sessionID: sessionID,
+					userName: "Client6",
+				},
+			],
+			done
+		);
+	});
+
+	after(function (done) {
+		disableLogs();
+		for (let c of clients) {
+			c.disconnect();
+		}
+		waitForClientDisconnects(done);
+	});
+
+	it(`6 clients with different userID should be connected.`, function (done) {
+		expect(Object.keys(Connections).length).to.equal(6);
+		ownerIdx = clients.findIndex((c) => c.query.userID == Sessions[sessionID].owner);
+		expect(ownerIdx).to.not.be.null;
+		expect(ownerIdx).to.not.be.undefined;
+		done();
+	});
+
+	const startTeamSealed = () => {};
+
+	const endTeamSealed = () => {
+		it("Owner ends the event.", function (done) {
+			this.timeout(4000);
+
+			for (let c = 0; c < clients.length; ++c) {
+				clients[c].once("endTeamSealed", function () {});
+			}
+			// TODO
+			done();
+		});
+	};
+});
