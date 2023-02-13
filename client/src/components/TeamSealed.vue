f2a8f9182dd0705e1a81dc0daebd9ee8198541d5
Senryoku <maretverdant@gmail.com>
2023-02-13 17:46:22 +0100

    Team Draft UI: Display some team information

--- client/src/components/TeamSealed.vue
+++ client/src/components/TeamSealed.vue
@@ -1,17 +1,27 @@
 <template>
 	<div class="team-sealed-container" :style="cssVariables">
-		<div class="section-title">
-			<h2>Team Sealed</h2>
-			<div>
-				Your Team:
-				<div v-for="user in teamDisplay" :key="user.idx">{{ user.userName }}</div>
+		<div class="team-sealed-infos">
+			<div class="team">
+				<div>Your Team:</div>
+				<div v-for="user in teamDisplay" :key="user.idx" :class="`player player-${user.idx}`">
+					<span>{{ user.userName }}</span>
+					<span class="color-list" v-if="user.colors">
+						<img
+							v-for="c in ['W', 'U', 'B', 'R', 'G'].filter((c) => user.colors[c] >= 3)"
+							:key="c"
+							:src="'img/mana/' + c + '.svg'"
+							class="mana-icon"
+						/>
+					</span>
+				</div>
 			</div>
+			<div>Click on cards to add them to your deck or return them to the common pool.</div>
 		</div>
 		<card-pool
 			:language="language"
 			:cards="state.cards"
 			:click="onCardClick"
-			:readOnly="true"
+			group="team-sealed"
 			:cardConditionalClasses="cardConditionalClasses"
 		></card-pool>
 	</div>
@@ -32,7 +42,7 @@ export default {
 			const variables = [];
 			for (let i = 0; i < this.state.team.length; ++i) {
 				const user = this.users.find((u) => u.userID === this.state.team[i]);
-				if (user) variables.push("--user-name-" + i + ': "' + user.userName + '";');
+				variables.push("--user-name-" + i + ': "' + (user?.userName ?? "(Disconnected)") + '";');
 			}
 			return variables.join(" ");
 		},
@@ -41,7 +51,16 @@ export default {
 
 			for (let i = 0; i < this.state.team.length; ++i) {
 				const user = this.users.find((u) => u.userID === this.state.team[i]);
-				r.push({ idx: i, userName: user ? user.userName : "(Disconnected)'" });
+				const colors = this.state.cards
+					.filter((c) => c.owner === this.state.team[i])
+					.reduce(
+						(acc, card) => {
+							for (let c of card.colors) acc[c] += 1;
+							return acc;
+						},
+						{ W: 0, U: 0, B: 0, R: 0, G: 0 }
+					);
+				r.push({ idx: i, userName: user ? user.userName : "(Disconnected)", colors });
 			}
 			return r;
 		},
@@ -59,6 +78,55 @@ export default {
 </script>
 
 <style scoped>
+.team-sealed-container {
+	--color-player-0: #8f2323;
+	--color-player-1: #23628f;
+	--color-player-2: #4f8f23;
+	--color-player-3: #8f6a23;
+	--color-player-4: #6b238f;
+}
+
+.team-sealed-infos {
+	display: flex;
+	justify-content: space-between;
+	margin: 1em;
+}
+
+.team {
+	display: flex;
+	gap: 1em;
+	align-items: baseline;
+}
+
+.player {
+	padding: 0.25em 0.5em;
+	background-color: #222;
+	border-radius: 0.25em;
+	display: inline-flex;
+	align-items: center;
+}
+.player-0 {
+	background-color: var(--color-player-0);
+}
+.player-1 {
+	background-color: var(--color-player-1);
+}
+.player-2 {
+	background-color: var(--color-player-2);
+}
+.player-3 {
+	background-color: var(--color-player-3);
+}
+.player-4 {
+	background-color: var(--color-player-4);
+}
+
+.color-list {
+	display: inline-flex;
+	gap: 0.2em;
+	margin-left: 0.5em;
+}
+
 :deep(.team-sealed-picked > *) {
 	filter: brightness(0.5);
 }
@@ -84,14 +152,6 @@ export default {
 :deep(.team-sealed-picked img) {
 	box-sizing: border-box;
 	border: 3px solid grey;
-}
-
-.team-sealed-container {
-	--color-player-0: #8f2323;
-	--color-player-1: #23628f;
-	--color-player-2: #4f8f23;
-	--color-player-3: #8f6a23;
-	--color-player-4: #6b238f;
 }
 
 :deep(.team-sealed-player-0 img) {
