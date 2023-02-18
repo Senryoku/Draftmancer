926dc41b567ae8acc169e699c49e8d73b855efa6
Senryoku <maretverdant@gmail.com>
2023-02-18 18:33:05 +0100

    Forgot this component.

--- client/src/components/DraftLogLive.vue
+++ client/src/components/DraftLogLive.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<template v-if="show">
-			<div v-if="player in draftlog.users">
+			<div v-if="player && player in draftlog.users">
 				<div class="draft-log-live-title">
 					<h2>Live Review: {{ draftlog.users[player].userName }}</h2>
 					<span v-if="player in draftlog.users && draftlog.users[player].picks.length > 0">
@@ -98,27 +98,33 @@
 	</div>
 </template>
 
-<script>
+<script lang="ts">
 import DraftLogPick from "./DraftLogPick.vue";
 import CardPool from "./CardPool.vue";
 import Card from "./Card.vue";
 import ScaleSlider from "./ScaleSlider.vue";
 import draggable from "vuedraggable";
+import { PropType } from "vue";
+import { Language } from "../../../src/Types";
+import { DraftLog, DraftPick } from "../../../src/DraftLog";
+import { pick } from "random-js";
+import { CardID, UniqueCard } from "../../../src/CardTypes";
+import { UserID } from "../../../src/IDTypes";
 
 export default {
 	name: "DraftLogLive",
 	components: { DraftLogPick, draggable, Card, CardPool, ScaleSlider },
 	props: {
 		show: { type: Boolean, default: true },
-		draftlog: { type: Object, required: true },
-		language: { type: String, required: true },
+		draftlog: { type: Object as PropType<DraftLog>, required: true },
+		language: { type: String as PropType<Language>, required: true },
 	},
 	data() {
 		return {
-			player: undefined,
+			player: undefined as UserID | undefined,
 			pack: 0,
 			pick: 0,
-			eventListeners: [],
+			eventListeners: [] as { element: HTMLElement; callback: () => void }[],
 			pickTransition: "right",
 			uniqueID: 0,
 			cardScale: 1,
@@ -135,12 +141,12 @@ export default {
 		document.removeEventListener("keydown", this.shortcuts);
 	},
 	methods: {
-		getCardName(cid) {
+		getCardName(cid: CardID) {
 			return this.language in this.draftlog.carddata[cid].printed_names
 				? this.draftlog.carddata[cid].printed_names[this.language]
 				: this.draftlog.carddata[cid].name;
 		},
-		setPlayer(userID) {
+		setPlayer(userID: UserID) {
 			if (!(userID in this.draftlog.users)) return;
 			this.player = userID;
 
@@ -171,17 +177,17 @@ export default {
 				++this.pick;
 			}
 		},
-		newPick(data) {
+		newPick(data: { userID: string; userName: string; cards: UniqueCard[] }) {
 			// Skip to last pick if we're spectating this player
 			if (data.userID === this.player) {
 				this.pack = this.picksPerPack.length - 1;
 				this.pick = this.picksPerPack[this.pack].length - 1;
 			}
 		},
-		generateCardArray(cardIDs) {
+		generateCardArray(cardIDs: CardID[]) {
 			return cardIDs.map((cid) => Object.assign({ uniqueID: ++this.uniqueID }, this.draftlog.carddata[cid]));
 		},
-		shortcuts(e) {
+		shortcuts(e: KeyboardEvent) {
 			if (e.key === "ArrowLeft") {
 				this.prevPick();
 				e.preventDefault();
@@ -193,11 +199,11 @@ export default {
 		registerPlayerSelectEvents() {
 			this.clearPlayerSelectEvents();
 			const self = this;
-			const playerEls = document.querySelectorAll("ul.player-list li");
+			const playerEls = document.querySelectorAll("ul.player-list li") as NodeListOf<HTMLElement>;
 			for (let el of playerEls) {
 				const callback = () => {
 					const id = el.dataset.userid;
-					self.setPlayer(id);
+					self.setPlayer(id!);
 				};
 				this.eventListeners.push({ element: el, callback: callback });
 				el.classList.add("clickable");
@@ -222,20 +228,22 @@ export default {
 				.join(", ");
 		},
 		selectedPlayerCards() {
-			if (this.draftlog.users?.[this.player]?.decklist?.main)
+			if (this.draftlog.users?.[this.player!]?.decklist?.main)
 				return {
-					main: this.generateCardArray(this.draftlog.users[this.player].decklist.main),
-					side: this.generateCardArray(this.draftlog.users[this.player].decklist.side),
+					main: this.generateCardArray(this.draftlog.users[this.player!].decklist!.main),
+					side: this.generateCardArray(this.draftlog.users[this.player!].decklist!.side),
 				};
 			return {
 				main: this.generateCardArray(
-					this.draftlog.users[this.player].picks.map((p) => p.pick.map((idx) => p.booster[idx])).flat()
+					(this.draftlog.users[this.player!].picks as DraftPick[])
+						.map((p: DraftPick) => p.pick.map((idx) => p.booster[idx]))
+						.flat()
 				),
 				side: [],
 			};
 		},
 		selectedLog() {
-			return this.draftlog.users[this.player];
+			return this.draftlog.users[this.player!];
 		},
 		picksPerPack() {
 			if (
@@ -246,24 +254,29 @@ export default {
 			)
 				return [];
 			// Infer PackNumber & PickNumber
-			let r = [];
+			let r: {
+				key: number;
+				data: DraftPick;
+				packNumber: number;
+				pickNumber: number;
+			}[][] = [];
 			let currPick = 0;
 			let currBooster = -1;
 			let lastSize = 0;
 			let currPickNumber = 0;
 			while (currPick < this.selectedLog.picks.length) {
-				if (this.selectedLog.picks[currPick].booster.length > lastSize) {
+				if ((this.selectedLog.picks[currPick] as DraftPick).booster.length > lastSize) {
 					++currBooster;
 					currPickNumber = 0;
 					r.push([]);
 				} else ++currPickNumber;
 				r[currBooster].push({
 					key: currPick,
-					data: this.selectedLog.picks[currPick],
+					data: this.selectedLog.picks[currPick] as DraftPick,
 					packNumber: currBooster,
 					pickNumber: currPickNumber,
 				});
-				lastSize = this.selectedLog.picks[currPick].booster.length;
+				lastSize = (this.selectedLog.picks[currPick] as DraftPick).booster.length;
 				++currPick;
 			}
 			return r;
