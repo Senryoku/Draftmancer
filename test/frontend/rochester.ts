227d7e8bb371a3bfa883eded2080ef94f9020ecf
Senryoku <maretverdant@gmail.com>
2023-03-17 19:25:06 +0100

    Update rochester front test

--- test/frontend/rochester.ts
+++ test/frontend/rochester.ts
@@ -1,24 +1,29 @@
 import { describe, it } from "mocha";
 import chai from "chai";
 const expect = chai.expect;
-import { waitAndClickXpath, waitAndClickSelector, pages, setupBrowsers } from "./src/common.js";
-import { Browser, Page } from "puppeteer";
+import { waitAndClickXpath, waitAndClickSelector, pages, setupBrowsers, PickResult } from "./src/common.js";
+import { Page } from "puppeteer";
+import { getRandom } from "../../src/utils.js";
 
-async function pickRochester(page: Page) {
-	let next = await page.waitForXPath(
-		"//div[contains(., 'Done drafting!')] | //span[contains(., 'Pick a card')] | //span[contains(., 'Waiting for')]"
-	);
-	let text = await page.evaluate((next) => (next as HTMLElement).innerText, next);
-	if (text === "Done drafting!") return true;
-	if (text.includes("Waiting for")) return false;
+export async function pickRochester(page: Page): Promise<PickResult> {
+	const done = await page.$$("xpath///h2[contains(., 'Done drafting!')]");
+	if (done.length > 0) return PickResult.Done;
+	const waiting = await page.$$("xpath///div[contains(., 'Waiting for')]");
+	if (waiting.length > 0) return PickResult.Waiting;
+	const cards = await page.$$(".booster:not(.booster-waiting) .booster-card");
+	if (cards.length === 0) return PickResult.Waiting;
 
-	const cards = await page.$$(".booster-card");
-	if (cards.length === 0) return false;
-	const card = cards[Math.floor(Math.random() * cards.length)];
+	const card = getRandom(cards);
 	expect(card).to.exist;
-	await card.click();
+	try {
+		await card.click();
+	} catch (e) {
+		// FIXME: IDK why this randomly fails.
+		return PickResult.Waiting;
+	}
+	while (await (await card.toElement("div")).evaluate((el) => !el.classList.contains("selected"))) await card.click();
 	await waitAndClickSelector(page, 'input[value="Confirm Pick"]');
-	return false;
+	return PickResult.Picked;
 }
 
 describe("Rochester", function () {
@@ -29,23 +34,29 @@ describe("Rochester", function () {
 		await pages[0].hover(".handle"); // Hover over "Other Game Modes"
 		await waitAndClickXpath(pages[0], "//button[contains(., 'Rochester')]");
 
-		for (let i = 0; i < pages.length; ++i)
-			await pages[i].waitForXPath("//h2[contains(., 'Rochester Draft')]", {
-				visible: true,
-			});
-		for (let i = 0; i < pages.length; ++i)
-			await pages[i].waitForXPath("//div[contains(., 'Draft Started!')]", {
-				hidden: true,
-			});
+		await Promise.all(
+			pages.map((p) =>
+				p.waitForXPath("//h2[contains(., 'Rochester Draft')]", {
+					visible: true,
+				})
+			)
+		);
+		await Promise.all(
+			pages.map((p) =>
+				p.waitForXPath("//div[contains(., 'Draft Started!')]", {
+					hidden: true,
+				})
+			)
+		);
 	});
 
 	it(`Pick until done.`, async function () {
 		this.timeout(100000);
-		let done = false;
-		while (!done) {
-			let ownerPromise = pickRochester(pages[0]);
-			let otherPromise = pickRochester(pages[1]);
-			done = (await ownerPromise) && (await otherPromise);
+		let done = [false, false];
+		while (done.some((d) => !d)) {
+			for (let i = 0; i < pages.length; i++) {
+				if (!done[i]) done[i] = (await pickRochester(pages[i])) === PickResult.Done;
+			}
 		}
 	});
 });
