afb6c7e58ba94360c467e8e1b0e9a2bd19abec5a
Senryoku <maretverdant@gmail.com>
2023-04-18 13:45:54 +0200

    [Client] Allow dismissing a card popup by click/touch. A card popup can be triggered on mobile by a longpress, this was unintended and we had no way to cleanly dispose of it.

--- client/src/components/CardPopup.vue
+++ client/src/components/CardPopup.vue
@@ -5,6 +5,7 @@
 			:class="{ left: position === 'left', right: position === 'right' }"
 			v-if="card"
 			v-show="display"
+			@pointerdown="close"
 		>
 			<div class="carousel">
 				<div
@@ -273,7 +274,6 @@ export default defineComponent({
 	top: 15vh;
 	height: var(--image-height);
 	z-index: 1040;
-	pointer-events: none;
 	filter: drop-shadow(0 0 0.5vw #000000);
 }
 
@@ -309,6 +309,7 @@ export default defineComponent({
 	position: relative;
 	width: 50vw;
 	height: var(--image-height);
+	pointer-events: none;
 }
 
 .carousel > .carousel-item {
