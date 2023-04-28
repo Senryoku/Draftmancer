652be064266a92c46cd33448460b4ed075b02895
Senryoku <maretverdant@gmail.com>
2023-04-28 09:54:38 +0200

    News & Patch Note

--- client/src/components/News.vue
+++ client/src/components/News.vue
@@ -1,6 +1,24 @@
 <template>
 	<div class="welcome-section">
 		<div class="news">
+			<em>April 28, 2023</em>
+			<ul>
+				<li>
+					New game mode: <strong>Solomon Draft</strong>!<br />A draft variant for two players reminiscing of
+					the card Fact or Fiction.
+				</li>
+				<li>Added a pick history for Rotisserie Drafts.</li>
+				<li>
+					Updated <a href="cubeformat.html#custom-cards" target="blank">Custom Cards support</a>:
+					<ul>
+						<li>Added an optional 'related_cards' property.</li>
+						<li>Improved mana cost parsing.</li>
+					</ul>
+				</li>
+				<li>Various tweaks and fixes.</li>
+			</ul>
+		</div>
+		<div class="news">
 			<em>April 16, 2023</em>
 			<ul>
 				<li>
@@ -20,71 +38,6 @@
 					Added support for
 					<img src="img/sets/mb1.svg" class="set-icon" style="--invertedness: 100%" /> Mystery Booster
 					convention editions and their playtest cards.
-				</li>
-				<li>
-					MTGADraft.tk will soon redirect to <a href="https://draftmancer.com">Draftmancer.com</a>. This means
-					that your Game Logs and settings will be reset if you are still using the old domain.
-					<strong>
-						Backup your logs and switch to <a href="https://draftmancer.com">Draftmancer.com</a> now to
-						avoid losing them!
-					</strong>
-				</li>
-			</ul>
-		</div>
-		<div class="news">
-			<em>April 10, 2023</em>
-			<p>MTGADraft.tk becomes <strong>Draftmancer.com</strong>!</p>
-			<p>
-				Nothing else changes, I'll keep the previous domain around for as long as possible, but
-				<a href="https://draftmancer.com">Draftmancer.com</a> is now the official URL.
-			</p>
-			<p>
-				Settings and, more importantly, Game Logs are tied to the domain. If you want to keep them,
-				<strong>Backup your Game Logs</strong> before switching to the new domain name.
-			</p>
-		</div>
-		<div class="news">
-			<em>April 5, 2023</em>
-			<p>
-				<img src="img/sets/mom.svg" class="set-icon" style="--invertedness: 100%; width: 2em" />
-				<strong>March of the Machine</strong> preliminary support.
-			</p>
-			<p>
-				As with all new sets, card data and collation will be refined in the coming days as new information is
-				revealed.
-			</p>
-			<p>
-				Quick reminder that maintaining this application and keeping up with WotC release schedule requires
-				time. If you use Draftmancer regularly, please consider donating a few bucks via
-				<a href="https://github.com/sponsors/Senryoku" target="_blank">
-					<font-awesome-icon icon="fa-brands fa-github"></font-awesome-icon> Github Sponsor
-				</a>
-				or
-				<a href="https://www.paypal.com/donate/?hosted_button_id=6L2CUS6DH82DL" target="_blank">
-					<font-awesome-icon icon="fa-brands fa-paypal"></font-awesome-icon> PayPal </a
-				>.<br />Thank you! — Sen
-			</p>
-		</div>
-		<div class="news">
-			<em>March 27, 2023</em>
-			<ul>
-				<li>
-					New game mode: <strong>Winchester Draft</strong>! A draft variant with elements from Winston and
-					Rochester drafts.
-				</li>
-				<li>The set selector has been updated: To select multiple sets, use Ctrl+Click or a longpress.</li>
-				<li>
-					You can now select a specific bonus sheet for
-					<img src="img/sets/sir.svg" class="set-icon" style="--invertedness: 100%" />
-					Shadows over Innistrad Remastered drafts (look for 'SIR: ...' in the 'More Sets...' menu).
-				</li>
-				<li>
-					A lot of work behind the scene went into updating the tech stack. <br />Hopefully you won't see many
-					differences - aside from some performance improvements, but I may have missed some regression: As
-					always please report those in the
-					<a href="https://discord.gg/XscXXNw" target="_blank"
-						><font-awesome-icon icon="fa-brands fa-discord"></font-awesome-icon> discord server</a
-					>!
 				</li>
 			</ul>
 		</div>
