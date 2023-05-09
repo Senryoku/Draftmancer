7887a4ab0900ef69e88e4514dd4b9c039b78164f
Senryoku <maretverdant@gmail.com>
2023-05-09 18:17:38 +0200

    Forgot to remove this.

--- src/draftQueue/QueueDescription.ts
+++ src/draftQueue/QueueDescription.ts
@@ -9,14 +9,3 @@ export type QueueDescription = {
 	description?: string;
 	setCode: SetCode;
 };
-
-export const AvailableQueues: readonly QueueDescription[] = [
-	{ id: "mom", name: "March of the Machine", playerCount: 8, setCode: "mom" },
-	{
-		id: "sir",
-		name: "Shadows over Innistrad Remastered",
-		playerCount: 8,
-		setCode: "sir",
-	},
-	{ id: "one", name: "Phyrexia: All Will Be One", playerCount: 8, setCode: "one" },
-];
