f44a537e3e8b469d894b6634cc370d37c3e20ef0
Senryoku <maretverdant@gmail.com>
2023-05-09 15:39:34 +0200

    DraftQueue: -SIR +DMU

--- src/draftQueue/AvailableQueues.ts
+++ src/draftQueue/AvailableQueues.ts
@@ -2,13 +2,9 @@ import { QueueDescription } from "./QueueDescription";
 
 export const AvailableQueues: readonly QueueDescription[] = [
 	{ id: "mom", name: "March of the Machine", playerCount: 8, setCode: "mom" },
-	{
-		id: "sir",
-		name: "Shadows over Innistrad Remastered",
-		playerCount: 8,
-		setCode: "sir",
-	},
+	// { id: "sir", name: "Shadows over Innistrad Remastered", playerCount: 8, setCode: "sir", },
 	{ id: "one", name: "Phyrexia: All Will Be One", playerCount: 8, setCode: "one" },
+	{ id: "dmu", name: "Dominaria United", playerCount: 8, setCode: "dmu" },
 ];
 
 export default AvailableQueues;
