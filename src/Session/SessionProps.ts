0f470961d831cdf986bed6efeb9b899696306abe
Senryoku <maretverdant@gmail.com>
2023-05-14 12:22:10 +0200

    Improved SessionProps type safety.

--- src/Session/SessionProps.ts
+++ src/Session/SessionProps.ts
@@ -1,11 +1,13 @@
 import Constants from "../Constants.js";
-import { isBoolean, isObject, isString } from "../TypeChecks.js";
+import { isArrayOf, isBoolean, isInteger, isObject, isString } from "../TypeChecks.js";
+import { SetCode } from "../Types";
+import { DistributionMode, DraftLogRecipients } from "./SessionTypes";
 
 // Validate session settings types and values.
-export const SessionsSettingsProps: { [propName: string]: (val: any) => boolean } = {
+export const SessionsSettingsProps: { [propName: string]: (val: unknown) => boolean } = {
 	ownerIsPlayer: isBoolean,
-	setRestriction(val: any) {
-		if (!Array.isArray(val)) return false;
+	setRestriction(val: unknown): val is SetCode[] {
+		if (!isArrayOf(isString)(val)) return false;
 		for (const s of val)
 			if (Constants.PrimarySets.indexOf(s) === -1) {
 				console.error(`Error: Set ${s} in not marked as primary.`);
@@ -16,72 +18,75 @@ export const SessionsSettingsProps: { [propName: string]: (val: any) => boolean 
 	isPublic: isBoolean,
 	description: isString,
 	ignoreCollections: isBoolean,
-	boostersPerPlayer(val: any) {
-		if (!Number.isInteger(val)) return false;
+	boostersPerPlayer(val: unknown) {
+		if (!isInteger(val)) return false;
 		return val >= 1;
 	},
-	cardsPerBooster(val: any) {
-		if (!Number.isInteger(val)) return false;
+	cardsPerBooster(val: unknown) {
+		if (!isInteger(val)) return false;
 		return val >= 1;
 	},
 	teamDraft: isBoolean,
 	randomizeSeatingOrder: isBoolean,
 	disableBotSuggestions: isBoolean,
-	bots(val: any) {
-		if (!Number.isInteger(val)) return false;
+	bots(val: unknown) {
+		if (!isInteger(val)) return false;
 		return val >= 0;
 	},
-	maxTimer(val: any) {
-		if (!Number.isInteger(val)) return false;
+	maxTimer(val: unknown) {
+		if (!isInteger(val)) return false;
 		return val >= 0;
 	},
-	maxPlayers(val: any) {
-		if (!Number.isInteger(val)) return false;
+	maxPlayers(val: unknown) {
+		if (!isInteger(val)) return false;
 		return val >= 1;
 	},
 	mythicPromotion: isBoolean,
 	useBoosterContent: isBoolean,
-	boosterContent(val: any) {
+	boosterContent(val: unknown) {
 		// Validate input (a value for each rarity and at least one card)
 		if (!isObject(val)) return false;
 		if (!["common", "uncommon", "rare", "bonus"].every((r) => r in val)) return false;
-		if (Object.values(val).some((i: any) => !Number.isInteger(i) || i < 0)) return false;
+		if (Object.values(val).some((i: unknown) => !isInteger(i) || i < 0)) return false;
 		if (Object.values(val).reduce((acc, val) => acc + val) <= 0) return false;
 		return true;
 	},
 	usePredeterminedBoosters: isBoolean,
 	colorBalance: isBoolean,
-	maxDuplicates(val: any) {
+	maxDuplicates(val: unknown) {
 		if (!isObject(val)) return false;
 		if (Object.values(val).some((i) => !Number.isInteger(i))) return false;
 		return true;
 	},
 	foil: isBoolean,
-	preferredCollation(val: any) {
+	preferredCollation(val: unknown): val is "Paper" | "MTGA" {
+		if (!isString(val)) return false;
 		return ["Paper", "MTGA"].includes(val);
 	},
 	useCustomCardList: isBoolean,
 	customCardListWithReplacement: isBoolean,
 	customCardList: isObject,
-	distributionMode(val: any) {
+	distributionMode(val: unknown): val is DistributionMode {
+		if (!isString(val)) return false;
 		return ["regular", "shufflePlayerBoosters", "shuffleBoosterPool"].includes(val);
 	},
-	customBoosters: Array.isArray,
+	customBoosters: isArrayOf(isString),
 	doubleMastersMode: isBoolean,
-	pickedCardsPerRound(val: any) {
-		if (!Number.isInteger(val)) return false;
+	pickedCardsPerRound(val: unknown) {
+		if (!isInteger(val)) return false;
 		return val >= 1;
 	},
-	burnedCardsPerRound(val: any) {
-		if (!Number.isInteger(val)) return false;
+	burnedCardsPerRound(val: unknown) {
+		if (!isInteger(val)) return false;
 		return val >= 0;
 	},
-	discardRemainingCardsAt(val: any) {
-		if (!Number.isInteger(val)) return false;
+	discardRemainingCardsAt(val: unknown) {
+		if (!isInteger(val)) return false;
 		return val >= 0;
 	},
 	personalLogs: isBoolean,
-	draftLogRecipients(val: any) {
+	draftLogRecipients(val: unknown): val is DraftLogRecipients {
+		if (!isString(val)) return false;
 		return ["everyone", "delayed", "owner", "none"].includes(val);
 	},
 	bracketLocked: isBoolean,
