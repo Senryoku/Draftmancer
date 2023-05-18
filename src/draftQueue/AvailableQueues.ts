c6fa54e9405556b6cfaaac73aa7c4b5d134a3cbc
Senryoku <maretverdant@gmail.com>
2023-05-18 20:56:35 +0200

    Init.

--- src/draftQueue/AvailableQueues.ts
+++ src/draftQueue/AvailableQueues.ts
@@ -1,11 +1,12 @@
 import { QueueDescription } from "./QueueDescription";
 
 export const AvailableQueues: readonly QueueDescription[] = [
-	{ id: "mat", name: "March of the Machine: the Aftermath", playerCount: 8, setCode: "mat" },
+	{ id: "ltr", name: "The Lord of the Rings: Tales of Middle-earth", playerCount: 8, setCode: "ltr" },
 	{ id: "mom", name: "March of the Machine", playerCount: 8, setCode: "mom" },
+	{ id: "one", name: "Phyrexia: All Will Be One", playerCount: 8, setCode: "one" },
+	// { id: "mat", name: "March of the Machine: the Aftermath", playerCount: 8, setCode: "mat" },
 	// { id: "sir", name: "Shadows over Innistrad Remastered", playerCount: 8, setCode: "sir", },
-	// { id: "one", name: "Phyrexia: All Will Be One", playerCount: 8, setCode: "one" },
-	{ id: "dmu", name: "Dominaria United", playerCount: 8, setCode: "dmu" },
+	// { id: "dmu", name: "Dominaria United", playerCount: 8, setCode: "dmu" },
 ];
 
 if (process.env.NODE_ENV !== "production") {
