68514032300b349059acc2c9a168dff1628f56dc
Senryoku <maretverdant@gmail.com>
2023-05-19 13:56:46 +0200

    Switch Draft Queue to MID for next MTGA event.

--- src/draftQueue/AvailableQueues.ts
+++ src/draftQueue/AvailableQueues.ts
@@ -3,9 +3,10 @@ import { QueueDescription } from "./QueueDescription";
 export const AvailableQueues: readonly QueueDescription[] = [
 	{ id: "mat", name: "March of the Machine: the Aftermath", playerCount: 8, setCode: "mat" },
 	{ id: "mom", name: "March of the Machine", playerCount: 8, setCode: "mom" },
+	{ id: "mid", name: "Innistrad: Midnight Hunt", playerCount: 8, setCode: "mid" },
 	// { id: "sir", name: "Shadows over Innistrad Remastered", playerCount: 8, setCode: "sir", },
 	// { id: "one", name: "Phyrexia: All Will Be One", playerCount: 8, setCode: "one" },
-	{ id: "dmu", name: "Dominaria United", playerCount: 8, setCode: "dmu" },
+	// { id: "dmu", name: "Dominaria United", playerCount: 8, setCode: "dmu" },
 ];
 
 if (process.env.NODE_ENV !== "production") {
