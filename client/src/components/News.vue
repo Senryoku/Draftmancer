d3f33498f2ca46a6282e319ac5fedb7410beca4c
Senryoku <maretverdant@gmail.com>
2023-08-24 21:41:42 +0200

    News & Patch Notes

--- client/src/components/News.vue
+++ client/src/components/News.vue
@@ -1,6 +1,21 @@
 <template>
 	<div class="welcome-section">
 		<div class="news">
+			<em>August 24, 2023</em>
+			<ul>
+				<li>
+					<img src="img/sets/woe.svg" class="set-icon" style="--invertedness: 100%" alt="" /> Wilds of
+					Eldraine is now available!
+				</li>
+				<li>Two new Grid Draft variants: Two players with two picks per grid and four players.</li>
+				<li>
+					New session setting "Automatic Logs Unlock Timer". This will automatically unlock the logs after a
+					customizable period of time if the owner forgot to do it.
+				</li>
+				<li>Usual tweaks and fixes.</li>
+			</ul>
+		</div>
+		<div class="news">
 			<em>July 23, 2023</em>
 			<p>
 				<img src="img/sets/cmm.svg" class="set-icon" style="--invertedness: 100%" alt="" /> Commander Masters
