ac734da4bc0d4c545591698a7603f8dd59f88536
Senryoku <maretverdant@gmail.com>
2024-01-02 12:33:34 +0100

    [Client] Vue & associates dep update

--- client/webpack.config.cjs
+++ client/webpack.config.cjs
@@ -78,6 +78,7 @@ module.exports = {
 		new webpack.DefinePlugin({
 			__VUE_OPTIONS_API__: true,
 			__VUE_PROD_DEVTOOLS__: false,
+			__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
 		}),
 	],
 };
