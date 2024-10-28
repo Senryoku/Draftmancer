7f240411bcb796f391ef05db0179c41f33dbcb53
Senryoku <maretverdant@gmail.com>
2024-10-29 00:07:50 +0100

    Prep J25 scripts (cards not on Scryfall yet)

---
+++ scripts/J25.py
@@ -0,0 +1,90 @@
+import os
+import requests
+import re 
+import json
+import urllib.parse
+import html
+import time
+
+PacketListURL = "https://magic.wizards.com/en/news/announcements/foundations-jumpstart-booster-themes"
+
+TitleRegex = r"<deck-list.* deck-title=\"(.*)\" format=\".*\">"
+CardRegex = r"(?:(\d+) )?(.+)"
+
+ImageNameRegex = re.compile("[^a-z]")
+
+Packets = []
+
+OutputFile = f'src/data/JumpstartFDN.json'
+if not os.path.isfile(OutputFile):
+    CardsByName = {}
+    CardsByID = {}
+    def getCardFromName(name):
+        if name in CardsByName:
+            return CardsByName[name]
+        name = html.unescape(name)
+        name = urllib.parse.quote(name)
+        r = requests.get(f"https://api.scryfall.com/cards/named?exact={name}&set=j25")
+        time.sleep(0.1)
+        if r.status_code != 200:
+            r = requests.get(f"https://api.scryfall.com/cards/named?exact={name}&set=fdn")
+            time.sleep(0.1)
+            if r.status_code != 200:
+                r = requests.get(f"https://api.scryfall.com/cards/named?exact={name}")
+                time.sleep(0.1)
+                if r.status_code != 200:
+                    print(f"Card not found: {name}")
+                    exit()
+        CardsByName[name] = r.json()
+        CardsByID[r.json()["id"]] = r.json()
+        return r.json()
+        
+        
+    NameFixes = {
+    }
+
+    def fix_cardname(n):
+        r = n.split(" //")[0].strip()
+        if (r in NameFixes):
+            return NameFixes[r]
+        return r
+
+    jumpInBoosters = []
+    page = requests.get(PacketListURL).text
+    matches = re.finditer(TitleRegex, page, re.MULTILINE)
+    matches_arr = []
+    for m in matches:
+        matches_arr.append(m)
+        
+    for idx in range(len(matches_arr)):
+        deck_name = matches_arr[idx].group(1)
+        print(f"Deck: {deck_name}")
+        
+        # find next occurrence of "<main-deck>"
+        start = page.find("<main-deck>", matches_arr[idx].span()[1]) + len("<main-deck>")
+        end = page.find("</main-deck>", start)
+        deck = page[start:end]
+        
+        colors = set()
+
+        cards_matches = re.findall(CardRegex, deck)
+        pack_cards = []
+        for c in cards_matches:
+            count = int(c[0]) if c[0] else 1
+            cardname = fix_cardname(c[1])
+            print(f"  {count} {cardname:<50}", end="")
+            card = getCardFromName(cardname)
+            for color in filter(lambda a: a in "WUBRG", card["color_identity"]):
+                colors.add(color)
+            for i in range(count):  # Add the card c[0] times
+                pack_cards.append(card["id"])
+            print(f" ({card['id']}, {card['set']})")
+        if len(pack_cards) == 0:
+            print("Error: Not cards?")
+            exit()
+
+        Packets.append({"name": deck_name, "image": ImageNameRegex.sub("", deck_name.lower()), "cards": pack_cards})
+        
+    with open(OutputFile, 'w', encoding="utf8") as outfile:
+        json.dump(jumpInBoosters, outfile, indent=4, ensure_ascii=False,)
+    print("Dumped to disk.")
