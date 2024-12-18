a7ad459e84e65d5fbd9646ac636750af1c435d1a
Senryoku <maretverdant@gmail.com>
2024-12-18 14:21:41 +0100

    JumpIn! FDN

--- scripts/JumpIn.py
+++ scripts/JumpIn.py
@@ -2,22 +2,38 @@
 # Retreive JumpIn pack information directly from Wizards' site
 import os
 import requests
-import re 
+import re
 import json
 import urllib.parse
 import html
 
 Sets = [
-    {"set": "dsk", "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-packets-update-for-duskmourn-house-of-horror"},
+    {"set": "fdn", "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-packets-update-for-foundations"},
+    {
+        "set": "dsk",
+        "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-packets-update-for-duskmourn-house-of-horror",
+    },
     {"set": "blb", "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-packets-update-for-bloomburrow"},
-    {"set": "otj", "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-packets-update-for-outlaws-of-thunder-junction"},
-    {"set": "mkm", "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-packets-update-for-murders-at-karlov-manor"},
-    {"set": "lci", "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-packets-update-for-the-lost-caverns-of-ixalan"},
+    {
+        "set": "otj",
+        "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-packets-update-for-outlaws-of-thunder-junction",
+    },
+    {
+        "set": "mkm",
+        "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-packets-update-for-murders-at-karlov-manor",
+    },
+    {
+        "set": "lci",
+        "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-packets-update-for-the-lost-caverns-of-ixalan",
+    },
     {"set": "woe", "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-update-for-wilds-of-eldraine"},
     {"set": "ltr", "url": "https://magic.wizards.com/en/news/mtg-arena/jump-into-middle-earth-on-mtg-arena"},
-    {"set": "one", "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-packets-update-for-phyrexia-all-will-be-one"},
+    {
+        "set": "one",
+        "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-packets-update-for-phyrexia-all-will-be-one",
+    },
     {"set": "bro", "url": "https://magic.wizards.com/en/news/mtg-arena/jump-in-packets-update-for-the-brothers-war"},
-    #{"set": "dmu", "url": "https://magic.wizards.com/en/news/mtg-arena/jump-packets-update-dominaria-united-2022-08-31"},
+    # {"set": "dmu", "url": "https://magic.wizards.com/en/news/mtg-arena/jump-packets-update-dominaria-united-2022-08-31"},
 ]
 
 TitleRegex = r"<deck-list.* deck-title=\"(.*)\" format=\".*\">"
@@ -32,8 +48,9 @@ for entry in Sets:
     CardsByName = {}
     CardsByID = {}
 
-    OutputFile = f'src/data/JumpInBoosters_{Set}.json'
+    OutputFile = f"src/data/JumpInBoosters_{Set}.json"
     if not os.path.isfile(OutputFile):
+
         def getCardFromName(name):
             if name in CardsByName:
                 return CardsByName[name]
@@ -49,12 +66,11 @@ for entry in Sets:
             CardsByID[r.json()["id"]] = r.json()
             return r.json()
 
-        NameFixes = {
-        }
+        NameFixes = {}
 
         def fix_cardname(n):
             r = n.split(" //")[0].strip()
-            if (r in NameFixes):
+            if r in NameFixes:
                 return NameFixes[r]
             return r
 
@@ -65,7 +81,7 @@ for entry in Sets:
         matches_arr = []
         for m in matches:
             matches_arr.append(m)
-            
+
         for idx in range(len(matches_arr)):
             deck_name = matches_arr[idx].group(1)
             print(f"Deck: {deck_name}")
@@ -74,7 +90,7 @@ for entry in Sets:
             start = page.find("<main-deck>", matches_arr[idx].span()[1]) + len("<main-deck>")
             end = page.find("</main-deck>", start)
             deck = page[start:end]
-            
+
             colors = set()
 
             cards_matches = re.findall(CardRegex, deck)
@@ -91,7 +107,7 @@ for entry in Sets:
             if len(set_cards) == 0:
                 print("Error: Not cards?")
                 exit()
-           
+
             start = page.find("<tbody>", matches_arr[idx].span()[1]) + len("<tbody>")
             end = page.find("</tbody>", start)
 
@@ -120,7 +136,7 @@ for entry in Sets:
 
             image = None
             image_rarity = None
-            rarity_values = {"common":0, "uncommon":1, "rare":2, "mythic":3, "special":4}
+            rarity_values = {"common": 0, "uncommon": 1, "rare": 2, "mythic": 3, "special": 4}
             for cid in set_cards:
                 c = CardsByID[cid]
                 if image == None or c["rarity"] > image_rarity and "image_uris" in c:
@@ -129,22 +145,31 @@ for entry in Sets:
 
             if image == None:
                 image = "/img/cardback.webp"
-                
+
             if os.path.isfile(f"client/public/img/jumpin/{deck_name.lower()}.png"):
                 image = f"/img/jumpin/{deck_name.lower()}.png"
 
-            jumpInBoosters.append({
-                "name": deck_name, 
-                "colors": list(colors), 
-                "cycling_land": False,
-                "image":  image, 
-                "cards": set_cards, 
-                "alts": altcards
-            })
+            jumpInBoosters.append(
+                {
+                    "name": deck_name,
+                    "colors": list(colors),
+                    "cycling_land": False,
+                    "image": image,
+                    "cards": set_cards,
+                    "alts": altcards,
+                }
+            )
         print(f"JumpIn Boosters: {len(jumpInBoosters)}")
         for c in jumpInBoosters:
-            print(f"{c['name']:<30}: {len(c['cards'])} + {len(c['alts'])} = {len(c['cards']) + len(c['alts'])} cards - {c['image']}")
+            print(
+                f"{c['name']:<30}: {len(c['cards'])} + {len(c['alts'])} = {len(c['cards']) + len(c['alts'])} cards - {c['image']}"
+            )
         if len(jumpInBoosters) > 0:
-            with open(OutputFile, 'w', encoding="utf8") as outfile:
-                json.dump(jumpInBoosters, outfile, indent=4, ensure_ascii=False,)
+            with open(OutputFile, "w", encoding="utf8") as outfile:
+                json.dump(
+                    jumpInBoosters,
+                    outfile,
+                    indent=4,
+                    ensure_ascii=False,
+                )
             print("Dumped to disk.")
