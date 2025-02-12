7048071fa9fcbc293a649b18f71f931623b02c69
Senryoku <maretverdant@gmail.com>
2025-02-12 09:29:34 +0100

    Allow special characters in sheet names

--- src/parseCardList.ts
+++ src/parseCardList.ts
@@ -718,8 +718,7 @@ export function parseCardList(
 				} else {
 					let sheet: Sheet = { collation: "random", cards: {} };
 					let sheetName = header;
-					const match =
-						/(?<name>[a-zA-Z0-9# ]*[a-zA-Z0-9#])\s*(?<count>\(\d+\))?\s*(?<settings>\{.*\})?/.exec(header);
+					const match = /^(?<name>[^"()[\]{}]+?)\s*(?<count>\(\d+\))?\s*(?<settings>\{.*\})?$/.exec(header);
 					if (match) {
 						const { name, count, settings } = match.groups!;
 						sheetName = name;
