fea34334a490fae71d2c36a826388df5168dd981
Senryoku <maretverdant@gmail.com>
2025-07-09 07:16:44 +0200

    EOE as primary

--- ManageCardData.py
+++ ManageCardData.py
@@ -1085,7 +1085,7 @@ constants["PrimarySets"] = [
     for s in PrimarySets
     if s in setinfos
     and s not in subsets
-    and s not in ["ren", "rin", "a22", "y22", "j22", "sis", "ltc", "who", "wot", "acr", "eoe", "tla", "spe"]
+    and s not in ["ren", "rin", "a22", "y22", "j22", "sis", "ltc", "who", "wot", "acr", "tla", "spe"]
 ]  # Exclude some codes that are actually part of larger sets (tsb, fmb1, h1r... see subsets), or aren't out yet
 with open("src/data/constants.json", "w", encoding="utf8") as constantsFile:
     json.dump(constants, constantsFile, ensure_ascii=False, indent=4)
