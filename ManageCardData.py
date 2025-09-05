2020af86699c19ec054353fa25e7710336e0fb39
Senryoku <maretverdant@gmail.com>
2025-09-05 22:51:30 +0200

    Data update

--- ManageCardData.py
+++ ManageCardData.py
@@ -1150,7 +1150,7 @@ constants["PrimarySets"] = [
     for s in PrimarySets
     if s in setinfos
     and s not in subsets
-    and s not in ["ren", "rin", "a22", "y22", "j22", "sis", "ltc", "who", "wot", "acr", "tla", "spe", "aa2"]
+    and s not in ["ren", "rin", "a22", "y22", "j22", "sis", "ltc", "who", "wot", "acr", "tla", "spe", "aa2", "mar"]
 ]  # Exclude some codes that are actually part of larger sets (tsb, fmb1, h1r... see subsets), or aren't out yet
 with open("src/data/constants.json", "w", encoding="utf8") as constantsFile:
     json.dump(constants, constantsFile, ensure_ascii=False, indent=4)
