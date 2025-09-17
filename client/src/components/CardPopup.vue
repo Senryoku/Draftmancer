ddb9a7d3141060c2da309842a4fb4cd67fb97d75
Senryoku <maretverdant@gmail.com>
2025-09-17 15:58:26 +0200

    [Client] Update CardPopup to script setup syntax

--- client/src/components/CardPopup.vue
+++ client/src/components/CardPopup.vue
@@ -83,8 +83,8 @@
 	</transition>
 </template>
 
-<script lang="ts">
-import { defineComponent, PropType } from "vue";
+<script setup lang="ts">
+import { ref, computed, getCurrentInstance } from "vue";
 import { Language } from "@/Types";
 import axios from "axios";
 
@@ -92,192 +92,190 @@ import CardText from "./CardText.vue";
 import CardImage from "./CardImage.vue";
 import { isString } from "../../../src/TypeChecks";
 import { Card, CardFace, CardID } from "@/CardTypes";
-import { ScryfallCard, isReady, CardCacheEntry } from "../vueCardCache";
+import { useCardCache, ScryfallCard, isReady, CardCacheEntry } from "../vueCardCache";
+const { cardCache } = useCardCache();
+
+import { useEmitter } from "../appCommon";
+const { emitter } = useEmitter();
 
 const scrollCooldown = 100; // ms
 
-export default defineComponent({
-	components: { CardImage, CardText },
-	props: {
-		language: { type: String as PropType<Language>, required: true },
-		customCards: { type: Object as PropType<Record<string, Card>> },
-	},
-	data() {
-		return {
-			display: false,
-			card: null as Card | null,
-			position: "left",
-			currentPart: 0,
-			lastScroll: 0,
-			displayCardText: false,
-			spellbooks: {} as { [name: string]: Set<CardID> }, // Associates card names to their spellbooks (Sets of card ids)
-		};
-	},
-	created() {
-		this.emitter.on("togglecardpopup", (event: MouseEvent, card: Card) => {
-			if (!this.display) {
-				this.position = event.clientX < window.innerWidth / 2 ? "right" : "left";
-				this.card = card;
-				this.currentPart = 0;
-				if (!this.card.is_custom) {
-					let promise = this.$cardCache.request(card.id);
-					// Also request associated spellbook if necessary
-					promise?.then(() => {
-						const cardData = this.$cardCache.get(card.id) as ScryfallCard;
-						if (!(card.name in this.spellbooks)) {
-							const url = `https://api.scryfall.com/cards/search?q=spellbook%3A%22${encodeURIComponent(
-								cardData.name
-							)}%22&unique=cards`;
-							axios
-								.get(url)
-								.then((response) => {
-									if (response.status === 200 && response.data?.data?.length > 0) {
-										this.spellbooks[cardData.name] = new Set();
-										for (const card of response.data.data) {
-											card.status = "ready";
-											this.$cardCache.add(card);
-											this.spellbooks[cardData.name].add(card.id);
-										}
-									}
-								})
-								.catch((error) => {
-									// There's no spellbook for this card, add an empty set so we don't request it again and return without error
-									if (error.response?.status === 404) {
-										this.spellbooks[cardData.name] = new Set();
-									} else console.error("Error fetching spellbook:", error);
-								});
-						}
-					});
+const props = defineProps<{
+	language: Language;
+	customCards?: Record<CardID, Card>;
+}>();
+
+const display = ref(false);
+const card = ref<Card | null>(null);
+const position = ref<"left" | "right">("left");
+const currentPart = ref(0);
+const lastScroll = ref(0);
+const displayCardText = ref(false);
+const spellbooks = ref<{ [name: string]: Set<CardID> }>({}); // Associates card names to their spellbooks (Sets of card ids)
+
+emitter.on("togglecardpopup", (event: MouseEvent, newCard: Card) => {
+	if (!display.value) {
+		position.value = event.clientX < window.innerWidth / 2 ? "right" : "left";
+		card.value = newCard;
+		currentPart.value = 0;
+		if (!card.value.is_custom) {
+			let promise = cardCache.request(card.value.id);
+			// Also request associated spellbook if necessary
+			promise?.then(() => {
+				const cardData = cardCache.get(newCard.id) as ScryfallCard;
+				if (!(newCard.name in spellbooks.value)) {
+					const url = `https://api.scryfall.com/cards/search?q=spellbook%3A%22${encodeURIComponent(
+						cardData.name
+					)}%22&unique=cards`;
+					axios
+						.get(url)
+						.then((response) => {
+							if (response.status === 200 && response.data?.data?.length > 0) {
+								spellbooks.value[cardData.name] = new Set();
+								for (const card of response.data.data) {
+									card.status = "ready";
+									cardCache.add(card);
+									spellbooks.value[cardData.name].add(card.id);
+								}
+							}
+						})
+						.catch((error) => {
+							// There's no spellbook for this card, add an empty set so we don't request it again and return without error
+							if (error.response?.status === 404) {
+								spellbooks.value[cardData.name] = new Set();
+							} else console.error("Error fetching spellbook:", error);
+						});
 				}
+			});
+		}
 
-				document.addEventListener("wheel", this.mouseWheel, { passive: false });
-				document.addEventListener("keydown", this.keyDown, { capture: true });
-				document.addEventListener("keyup", this.keyUp, { capture: true });
-				this.display = true;
-			} else this.close();
-		});
-		this.emitter.on("closecardpopup", () => {
-			this.close();
-		});
-	},
-	methods: {
-		close() {
-			this.display = false;
-			this.displayCardText = false;
-			this.cleanupEventHandlers();
-		},
-		hasPendingData() {
-			if (!this.card || this.card?.is_custom) return false;
-			return this.$cardCache.get(this.card.id)?.status === "pending";
-		},
-		nextPart() {
-			if (this.relatedCards.length === 0) return;
-			this.currentPart = (this.currentPart + 1) % (this.relatedCards.length + 1);
-		},
-		previousPart() {
-			if (this.relatedCards.length === 0) return;
-			this.currentPart = (this.currentPart - 1) % (this.relatedCards.length + 1);
-			while (this.currentPart < 0) this.currentPart += this.relatedCards.length + 1;
-		},
-		mouseWheel(event: WheelEvent) {
-			if (this.relatedCards.length > 0) {
-				if (Date.now() - this.lastScroll > scrollCooldown) {
-					if (event.deltaY > 0) this.nextPart();
-					else this.previousPart();
-					this.lastScroll = Date.now();
-				}
-				event.stopPropagation();
-				event.preventDefault();
-			}
-		},
-		keyDown(event: KeyboardEvent) {
-			switch (event.key) {
-				case "Escape":
-					this.close();
-					break;
-				case "ArrowUp":
-					this.previousPart();
-					break;
-				case "ArrowDown":
-					this.nextPart();
-					break;
-				case "Alt":
-					this.displayCardText = event.altKey;
-					this.$forceUpdate();
-					break;
-				default:
-					// Ignore this event
-					return;
-			}
-			// We handled it.
-			event.stopPropagation();
-			event.preventDefault();
-		},
-		keyUp(event: KeyboardEvent) {
-			switch (event.key) {
-				case "Alt":
-					this.displayCardText = event.altKey;
-					this.$forceUpdate();
-					break;
-				default:
-					// Ignore this event
-					return;
-			}
-			// We handled it.
-			event.stopPropagation();
-			event.preventDefault();
-		},
-		negMod(x: number, n: number) {
-			return ((x % n) + n) % n;
-		},
-		cleanupEventHandlers() {
-			document.removeEventListener("wheel", this.mouseWheel);
-			document.removeEventListener("keydown", this.keyDown, { capture: true });
-			document.removeEventListener("keyup", this.keyUp, { capture: true });
-		},
-		relatedCardImage(index: number) {
-			if (this.relatedCards.length <= index) return undefined;
-			const card = this.relatedCards[index];
-			if (card.status === "custom") return card.image_uris["en"]!;
-			if (!isReady(card)) return undefined;
-			return card.image_uris
-				? card.image_uris.border_crop
-				: card.card_faces && card.card_faces[0] && card.card_faces[0].image_uris
-					? card.card_faces[0].image_uris.border_crop
-					: undefined;
-		},
-	},
-	computed: {
-		relatedCards() {
-			let r: CardCacheEntry[] = [];
-			if (!this.card) return r;
-			if (this.card.is_custom) {
-				if (this.customCards && this.card.related_cards)
-					return [
-						...this.card.related_cards.map((c) => {
-							if (isString(c)) {
-								if (this.customCards![c]) return { status: "custom", ...this.customCards![c] };
-								else return this.$cardCache.get(c);
-							} else return { status: "custom", ...c };
-						}),
-					] as (({ status: "custom"; id: string } & CardFace) | CardCacheEntry)[];
-			} else {
-				const c = this.$cardCache.get(this.card.id);
-				if (!isReady(c)) return r;
+		document.addEventListener("wheel", mouseWheel, { passive: false });
+		document.addEventListener("keydown", keyDown, { capture: true });
+		document.addEventListener("keyup", keyUp, { capture: true });
+		display.value = true;
+	} else close();
+});
+emitter.on("closecardpopup", close);
 
-				if (c.all_parts?.length > 0)
-					for (let card of c.all_parts) {
-						if (card.id !== this.card.id) r.push(this.$cardCache.get(card.id));
-					}
-				if (this.spellbooks[this.card?.name]?.size > 0)
-					for (const cid of this.spellbooks[this.card.name]) r.push(this.$cardCache.get(cid));
+function close() {
+	display.value = false;
+	displayCardText.value = false;
+	cleanupEventHandlers();
+}
 
-				if (this.card.related_cards)
-					for (const cid of this.card.related_cards) if (isString(cid)) r.push(this.$cardCache.get(cid));
+function hasPendingData() {
+	if (!card.value || card.value?.is_custom) return false;
+	return cardCache.get(card.value.id)?.status === "pending";
+}
+
+function nextPart() {
+	if (relatedCards.value.length === 0) return;
+	currentPart.value = (currentPart.value + 1) % (relatedCards.value.length + 1);
+}
+
+function previousPart() {
+	if (relatedCards.value.length === 0) return;
+	currentPart.value = (currentPart.value - 1) % (relatedCards.value.length + 1);
+	while (currentPart.value < 0) currentPart.value += relatedCards.value.length + 1;
+}
+
+function mouseWheel(event: WheelEvent) {
+	if (relatedCards.value.length > 0) {
+		if (Date.now() - lastScroll.value > scrollCooldown) {
+			if (event.deltaY > 0) nextPart();
+			else previousPart();
+			lastScroll.value = Date.now();
+		}
+		event.stopPropagation();
+		event.preventDefault();
+	}
+}
+
+function keyDown(event: KeyboardEvent) {
+	switch (event.key) {
+		case "Escape":
+			close();
+			break;
+		case "ArrowUp":
+			previousPart();
+			break;
+		case "ArrowDown":
+			nextPart();
+			break;
+		case "Alt":
+			displayCardText.value = event.altKey;
+			getCurrentInstance()?.proxy?.$forceUpdate();
+			break;
+		default:
+			// Ignore this event
+			return;
+	}
+	// We handled it.
+	event.stopPropagation();
+	event.preventDefault();
+}
+
+function keyUp(event: KeyboardEvent) {
+	switch (event.key) {
+		case "Alt":
+			displayCardText.value = event.altKey;
+			getCurrentInstance()?.proxy?.$forceUpdate();
+			break;
+		default:
+			// Ignore this event
+			return;
+	}
+	// We handled it.
+	event.stopPropagation();
+	event.preventDefault();
+}
+
+function cleanupEventHandlers() {
+	document.removeEventListener("wheel", mouseWheel);
+	document.removeEventListener("keydown", keyDown, { capture: true });
+	document.removeEventListener("keyup", keyUp, { capture: true });
+}
+
+function relatedCardImage(index: number) {
+	if (relatedCards.value.length <= index) return undefined;
+	const card = relatedCards.value[index];
+	if (card.status === "custom") return card.image_uris["en"]!;
+	if (!isReady(card)) return undefined;
+	return card.image_uris
+		? card.image_uris.border_crop
+		: card.card_faces && card.card_faces[0] && card.card_faces[0].image_uris
+			? card.card_faces[0].image_uris.border_crop
+			: undefined;
+}
+
+const relatedCards = computed(() => {
+	let r: CardCacheEntry[] = [];
+	if (!card.value) return r;
+	if (card.value.is_custom) {
+		if (props.customCards && card.value.related_cards)
+			return [
+				...card.value.related_cards.map((c) => {
+					if (isString(c)) {
+						if (props.customCards![c]) return { status: "custom", ...props.customCards![c] };
+						else return cardCache.get(c);
+					} else return { status: "custom", ...c };
+				}),
+			] as (({ status: "custom"; id: string } & CardFace) | CardCacheEntry)[];
+	} else {
+		const c = cardCache.get(card.value.id);
+		if (!isReady(c)) return r;
+
+		if (c.all_parts?.length > 0)
+			for (let part of c.all_parts) {
+				if (part.id !== card.value.id) r.push(cardCache.get(part.id));
 			}
-			return r;
-		},
-	},
+		if (spellbooks.value[card.value?.name]?.size > 0)
+			for (const cid of spellbooks.value[card.value.name]) r.push(cardCache.get(cid));
+
+		if (card.value.related_cards)
+			for (const cid of card.value.related_cards) if (isString(cid)) r.push(cardCache.get(cid));
+	}
+	return r;
 });
 </script>
 
